056e6f2ec6863d6373754032a05ed592a9b5874b
nacho <nachofvillar@gmail.com>
2021-04-14 13:05:45 +0200

    creado componente jornada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JornadaComponent } from './jornada/jornada.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JornadaComponent
   ],
   imports: [
     BrowserModule,
