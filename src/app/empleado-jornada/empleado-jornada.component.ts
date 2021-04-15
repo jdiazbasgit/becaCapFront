83e712277a27f8d5ea6a862c912e48fe73c54555
SergioMarquesMenendez <sergimarques@hotmail.com>
2021-04-15 13:59:34 +0200

    15/04/2021 mañana

--- src/app/empleado-jornada/empleado-jornada.component.ts
+++ src/app/empleado-jornada/empleado-jornada.component.ts
@@ -1,5 +1,7 @@
+import { VariableBinding } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { EmpleadoDto, JornadaDto, ServiceService } from '../service.service';
+import { Observable } from 'rxjs';
+import { Empleado, Jornada, ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-empleado-jornada',
@@ -8,17 +10,36 @@ import { EmpleadoDto, JornadaDto, ServiceService } from '../service.service';
 })
 export class EmpleadoJornadaComponent implements OnInit {
 
-  public datosEmp: EmpleadoDto[] = []
-  public datosJor: JornadaDto[] = []
+  public datosEmp: Observable<Empleado> 
+  public datosJor: Array<Jornada> = new Array()
+  public datosJorEmp: Array<Jornada> = new Array()
   
   constructor(private dataService : ServiceService) { 
     
     this.datosEmp = this.dataService.getDatosEmpleados();
     this.datosJor = this.dataService.getDatosJornadas();
+    this.datosJorEmp=this.dataService.getIdEmpleados()
+   
   }
 
   ngOnInit(): void {
-    
-  }
+   // console.log(this.datosEmp, this.datosJor, this.datosJorEmp)
+    this.cargaInicial()
 
+  }
+  cargaInicial(): void{
+    let jornada: string
+    console.log(this.dataService.getDatosEmpleados())
+    this.dataService.getDatosEmpleados().forEach((elements: any) => {
+      
+      // this.dataService.getDatos(element.jornada.href).subscribe((jornada:any)=>{
+      //   console.log(element)
+      //   jornada=element._links.self.href.substring(element._links.self.href.length-1)
+      // })
+      // let empleado:Empleado= new Empleado(element.nombre, element.apellidos,element.dni,
+      //    element.identificador,element.fecha_alta, element.fecha_baja, jornada)
+      //    this.datosEmp.push(empleado)
+         console.log("empleados: "+elements)
+    });
+  }
 }
--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-  urlJornada = "./assets/jornadas.json"
-  urlEmpleados = "./assets/empleados.json"
+  urlJornada = "./assets/days.json"
+  urlEmpleados = "./assets/employees.json"
+  jornadaLinks: any;
   constructor(private httpClient: HttpClient) { }
 
   private getDatosJornada() {
@@ -17,37 +19,74 @@ export class ServiceService {
     return this.httpClient.get(this.urlEmpleados)
   }
 
+  private getIdEmpleado() {
+    return this.httpClient.get(this.jornadaLinks)
+  }
 
-  public getDatosEmpleados(): EmpleadoDto[] {
-    let datosEmp: EmpleadoDto[] = []
+
+  public getDatosEmpleados(): Observable<Empleado> {
+    let datosEmp: Observable<Empleado>
     let empleados = this.getDatosEmpleado()
 
-    empleados.forEach((element: any) => {
-      console.log(element);
-      element.forEach((datos: any) => {
-        let emp = new EmpleadoDto(datos.nombre, datos.apellidos, datos.dni, datos.jornada)
-        datosEmp.push(emp)
+    empleados.subscribe((element: any) => {
+       console.log(element);
+      element._embedded.employees.forEach((dato: any) => {
+        // let links = new Links(datos._links.self.href, datos._links.employee.href,
+        //    datos.link.jornada.href)
+        // console.log(datos._links.jornada)
+        let emp = new Empleado(dato.nombre, dato.apellidos, dato.dni, dato.identificador,
+          dato.fecha_alta, dato.fecha_baja, dato._links.jornada)
+          //console.log(emp)
+        this.jornadaLinks = dato._links.jornada.href
+        //this.urlJornada = datos._links.jornada.href
+        //console.log(this.jornadaLinks)
+        //datosEmp.push(emp)
+        //console.log(element)
+        datosEmp=element
+        //return element
       });
     })
     // .then(() => { return datosEmp; })
-    return  datosEmp;
+    return datosEmp;
   }
 
-  public getDatosJornadas(): JornadaDto[]{
-    let datosJor: JornadaDto[] = []
-    let jornadas = this.getDatosJornada()
 
+  public getDatosJornadas(): Jornada[] {
+    let datosJor: Jornada[] = []
+    let jornadas = this.getDatosJornada()
     jornadas.forEach((element: any) => {
-      element.forEach((datos:any) => {
-        let jor = new JornadaDto(datos.id, datos.descripcion);
+      //console.log(element)
+      element._embedded.days.forEach((datos: any) => {
+        let jor = new Jornada(datos.lunes, datos.martes, datos.miercoles,
+          datos.jueves, datos.viernes, datos.sabado, datos.domingo, datos.descripcion,
+          datos.especial, datos._links);
+        //console.log(jor)
         datosJor.push(jor)
       });
     })
-    
+
     return datosJor
+  }
+  public getIdEmpleados(): Jornada[] {
+    let id = this.getIdEmpleado()
+    let datosJorEmp: Jornada[] = []
+    id.forEach((datos: any) => {
+      let jor = new Jornada(datos.lunes, datos.martes, datos.miercoles,
+        datos.jueves, datos.viernes, datos.sabado, datos.domingo, datos.descripcion,
+        datos.especial, datos._links);
+      datosJorEmp.push(jor)
+    });
+    return datosJorEmp
+  }
+
+  public getDatos(url: string): Observable<any> {
+
+    return this.httpClient.get(url)
   }
 
 }
+
+
 
 // cambiar(): void {
 //   this.uno.user = "federico";
@@ -58,15 +97,32 @@ export class ServiceService {
 //   })
 // }
 
-export class EmpleadoDto {
-  constructor(public nombre: string, public apellidos: string, public dni: string, public jornada: number) {
+export class Empleado {
+  constructor(public nombre: string, public apellidos: string, public dni: string,
+    public identificador: string,
+    public fecha_alta: Date, public fecha_baja: Date, public jornada: string) {
 
   }
 }
 
-export class JornadaDto {
-  constructor(public id: number, public descripcion: string) {
+export class Jornada {
+  constructor(public lunes: string, public martes: string, public miercoles: string,
+    public jueves: string, public viernes: string, public sabado: string, public domingo: string,
+    public descripcion: string, public especial: number, public _links: LinksJor) {
 
   }
 }
+
+export class Links {
+  constructor(public self: string, public employee: string, public jornada: string) {
+
+  }
+}
+
+export class LinksJor {
+  constructor(public self: string, public day: string) {
+
+  }
+}
+
 
