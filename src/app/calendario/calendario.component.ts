1a17283ae70c55af0608ae84bb825bdbb62cba71
Salgado Uralde <inaki.salgado-uralde@capgemini.com>
2021-04-16 13:51:40 +0200

    componente DIA funcionando

--- src/app/calendario/calendario.component.ts
+++ src/app/calendario/calendario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarioServiceService } from '../calendario-service.service';
 
 @Component({
   selector: 'app-calendario',
@@ -7,11 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarioComponent implements OnInit {
 
-  constructor() { }
+  url: string = './assets/calendarsBBDD.json';
+  url2: string = 'http://188.127.162.129:8080/api/calendars';
+  dias: Dia[] = [];
+
+  constructor(private calendarioService: CalendarioServiceService) { }
 
   ngOnInit(): void {
+    this.getDias(this.dias, this.calendarioService);
+  }
+
+  getDias(dias: Dia[], service) {
+    service.getDatos(this.url).subscribe(function (datos: any) {
+      datos._embedded.calendars.forEach((element: any) => {
+        //service.getDatos(element._links.estado.href).subscribe(function (estado: any) {
+        //  console.log(estado);
+        //})
+        let dia: Dia;
+        //dia = new Dia(element.fecha, estado.descripcion, estado.tipo);
+        dias.push(dia);
+      });
+    });
   }
 
 }
 
+export class Dia {
+  fecha: Date;
+  estadoDescripcion: string;
+  estadoTipo: number;
+  constructor(fecha: Date, estadoDescripcion: string, estadoTipo: number) {
+    this.fecha = fecha;
+    this.estadoDescripcion = estadoDescripcion;
+    this.estadoTipo = estadoTipo;
+  }
 
+}
--- src/app/dia/dia.component.ts
+++ src/app/dia/dia.component.ts
@@ -1,51 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { CalendarioServiceService } from "../calendario-service.service";
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dia',
   templateUrl: './dia.component.html',
-  styleUrls: ['./dia.component.css']
+  styleUrls: ['./dia.component.css'],
 })
 export class DiaComponent implements OnInit {
-url: string = './assets/calendarsBBDD.json';
-url2: string = 'http://188.127.162.129:8080/api/calendars';
-dias: Dia[] = [];
-
-  constructor(private calendarioService: CalendarioServiceService) { }
+  @Input() numero: number;
+  @Input() tipoDia: string;
+  constructor() { }
 
   ngOnInit(): void {
-    this.getDias(this.dias)
   }
 
-  getDias(dias: Dia[]) {
-    this.calendarioService.getDatos(this.url).subscribe(function (datos: any) {
-      datos._embedded.calendars.forEach((element: any) => {
-        this.getEstados(element)
-        let dia: Dia;
-        dia = new Dia(
-          element.fecha,       
-          element.apellidos,
-          element.dni,
-        );
-        dias.push(dia);
-      });
-    });
-  }
-  getEstados(element: any){
-        this.calendarioService.getDatos(element._links.estado.href).subscribe(function (estados: any) {
-          console.log(estados)
-        })
-  }
-
-}
-
-export class Dia {
-  fecha: Date
-  estadoDescripcion: string
-  estadoTipo: number
-  constructor(fecha: Date, estadoDescripcion: string, estadoTipo: number){
-    this.fecha = fecha
-    this.estadoDescripcion = estadoDescripcion
-    this.estadoTipo = estadoTipo
+  cambiarTipoDia() {
+    let aux: any
+    aux = document.getElementById("selectTipoDia")
+    this.tipoDia = aux.value
   }
 }
