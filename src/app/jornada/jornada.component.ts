fab23e8fe0a7cd3ef2e562978ded44a6e5d14a2f
Ilab Sentuser <ilabsentuser@gmail.com>
2021-04-29 17:06:12 +0200

    first trys at data modification

--- src/app/jornada/jornada.component.ts
+++ src/app/jornada/jornada.component.ts
@@ -1,6 +1,6 @@
-import { HtmlParser } from '@angular/compiler';
 import { AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
+import { element } from 'protractor';
 import { Jornada, JornadaDatosService } from '../jornada-datos.service';
 
 @Component({
@@ -12,9 +12,11 @@ export class JornadaComponent implements AfterViewInit {
 
   @ViewChild('modalTemplate') modalTemplate: TemplateRef<any>;
 
+  option;
+  modalRef;
   operation: string = "Nueva";
   service: any;
-  url: string = "http://188.127.162.129:8080/api/jornadas/";
+  url: string = "http://10.68.9.250/api/jornadas/";
   jornadas: Jornada[] = [];
 
   constructor(service: JornadaDatosService, config: NgbModalConfig, private modal: NgbModal, private datosService: JornadaDatosService) {
@@ -36,13 +38,13 @@ export class JornadaComponent implements AfterViewInit {
           jornada = new Jornada(
             element.id,
             this.treatDay(element.lunes),
-            this.treatDay(element.martes), 
-            this.treatDay(element.miercoles), 
-            this.treatDay(element.jueves), 
-            this.treatDay(element.viernes), 
-            this.treatDay(element.sabado), 
-            this.treatDay(element.domingo), 
-            element.descripcion, 
+            this.treatDay(element.martes),
+            this.treatDay(element.miercoles),
+            this.treatDay(element.jueves),
+            this.treatDay(element.viernes),
+            this.treatDay(element.sabado),
+            this.treatDay(element.domingo),
+            element.descripcion,
             this.treatSpecial(element.especial));
           jornadas.push(jornada);
         });
@@ -71,93 +73,98 @@ export class JornadaComponent implements AfterViewInit {
     return result;
   }
 
-/*
-  showJornada(): void {
-    let ident: number = 0;
-    this.service.getDatos(this.url)
-      .then((datos: any) => {
-        datos.forEach((element: any) => {
-          this.treatSemana(element, ident);
-          ident++;
-        });
-      })
-  }
-
-
-  treatSemana(semana: any, ident: number) {
-    let cuerpoTabla = document.querySelector("#filasJornada");
-    let fila = document.createElement("tr");
-    fila.classList.add("text-center");
-    fila.id = `fila${ident}`;
-
-    let nombre = document.createElement("td");
-    nombre.innerText = semana.descripcion;
-
-
-    let lunes = document.createElement("td");
-    lunes.innerText = this.treatDay(semana.lunes);
-
-    let martes = document.createElement("td");
-    martes.innerText = this.treatDay(semana.martes);
-
-    let miercoles = document.createElement("td");
-    miercoles.innerText = this.treatDay(semana.miercoles);
-
-    let jueves = document.createElement("td");
-    jueves.innerText = this.treatDay(semana.jueves);
-
-    let viernes = document.createElement("td");
-    viernes.innerText = this.treatDay(semana.viernes);
-
-    let sabado = document.createElement("td");
-    sabado.innerText = this.treatDay(semana.sabado);
-
-    let domingo = document.createElement("td");
-    domingo.innerText = this.treatDay(semana.domingo);
-
-    let especial = document.createElement("td");
-    especial.innerText = this.treatSpecial(semana.especial);
-
-    let colBoton = document.createElement("td");
-    let botonEdit = document.createElement("button");
-    botonEdit.classList.add("btn");
-    botonEdit.classList.add("btn-info");
-    botonEdit.innerText = "Edit";
-
-    var self = this;
-    botonEdit.addEventListener('click', function () {
-      self.showModal(self.modalTemplate, 'edit');
-    });
-
-    colBoton.appendChild(botonEdit);
-
-    fila.appendChild(nombre);
-    fila.appendChild(lunes);
-    fila.appendChild(martes);
-    fila.appendChild(miercoles);
-    fila.appendChild(jueves);
-    fila.appendChild(viernes);
-    fila.appendChild(sabado);
-    fila.appendChild(domingo);
-    fila.appendChild(especial);
-    fila.appendChild(colBoton);
-
-    cuerpoTabla.appendChild(fila);
-
-  }
-*/
+  /*
+    showJornada(): void {
+      let ident: number = 0;
+      this.service.getDatos(this.url)
+        .then((datos: any) => {
+          datos.forEach((element: any) => {
+            this.treatSemana(element, ident);
+            ident++;
+          });
+        })
+    }
+  
+  
+    treatSemana(semana: any, ident: number) {
+      let cuerpoTabla = document.querySelector("#filasJornada");
+      let fila = document.createElement("tr");
+      fila.classList.add("text-center");
+      fila.id = `fila${ident}`;
+  
+      let nombre = document.createElement("td");
+      nombre.innerText = semana.descripcion;
+  
+  
+      let lunes = document.createElement("td");
+      lunes.innerText = this.treatDay(semana.lunes);
+  
+      let martes = document.createElement("td");
+      martes.innerText = this.treatDay(semana.martes);
+  
+      let miercoles = document.createElement("td");
+      miercoles.innerText = this.treatDay(semana.miercoles);
+  
+      let jueves = document.createElement("td");
+      jueves.innerText = this.treatDay(semana.jueves);
+  
+      let viernes = document.createElement("td");
+      viernes.innerText = this.treatDay(semana.viernes);
+  
+      let sabado = document.createElement("td");
+      sabado.innerText = this.treatDay(semana.sabado);
+  
+      let domingo = document.createElement("td");
+      domingo.innerText = this.treatDay(semana.domingo);
+  
+      let especial = document.createElement("td");
+      especial.innerText = this.treatSpecial(semana.especial);
+  
+      let colBoton = document.createElement("td");
+      let botonEdit = document.createElement("button");
+      botonEdit.classList.add("btn");
+      botonEdit.classList.add("btn-info");
+      botonEdit.innerText = "Edit";
+  
+      var self = this;
+      botonEdit.addEventListener('click', function () {
+        self.showModal(self.modalTemplate, 'edit');
+      });
+  
+      colBoton.appendChild(botonEdit);
+  
+      fila.appendChild(nombre);
+      fila.appendChild(lunes);
+      fila.appendChild(martes);
+      fila.appendChild(miercoles);
+      fila.appendChild(jueves);
+      fila.appendChild(viernes);
+      fila.appendChild(sabado);
+      fila.appendChild(domingo);
+      fila.appendChild(especial);
+      fila.appendChild(colBoton);
+  
+      cuerpoTabla.appendChild(fila);
+  
+    }
+  */
 
   showModal(template, operation, id = 0) {
-    this.modal.open(template, { size: 'lg' });
-/************* 
-    
-  Aqui se tiene que checkear que no haya algun undefined o null en algun dia de la semana
-  porque sino en el td.innerHtml dara error. Por ejemplo probar con el registro A que tiene 
-  vacios los dias. Importante acordarse de que puede haber dias bien pero alguno null entre medias.
-
-**************/
-    this.tableGenerator(operation,id);
+    this.modalRef = this.modal.open(template, { size: 'lg' });
+    this.tableGenerator(operation, id);
     this.tableUpdater(2);
+
+    this.modalRef.result.then((result) => {
+      let tbody = <HTMLElement>result;
+
+      let turns = Array();
+      let i;
+      for (i = 0; i < this.option; i++)
+        Array.from(tbody.getElementsByClassName(`t${i}`)).forEach((element)=>{
+          turns.push(<HTMLElement> element);
+        })
+      this.generateWorkday(turns);
+    })
   }
 
   tableUpdater(opt) {
@@ -186,7 +193,7 @@ export class JornadaComponent implements AfterViewInit {
           (<HTMLInputElement>element).disabled = false;
         });
     }
-
+    this.option = opt;
   }
 
   tableGenerator(operation, id) {
@@ -194,17 +201,22 @@ export class JornadaComponent implements AfterViewInit {
     let days = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
 
     if (operation == 'edit') {
+      id++; //esto se hace porque los ids empiezan en 1 y los indices de las filas en 0.
       //this.service.getDatos(this.url2).then((datos: any) => {
-        this.service.getDatos(this.url + id).then((datos: any) => {
+      this.service.getDatos(this.url + id).then((datos: any) => {
         let i;
         let max = 1;
         for (i = 0; i < 7; i++) {
           let jornada = datos[days[i]].split("&");
-          console.log(jornada);
-          if (jornada.length > max)
-            max = jornada.length;
 
-          tbody.appendChild(this.rowGenerator(i, jornada));
+          if (jornada == undefined)
+            tbody.appendChild(this.rowGenerator(i));
+          else {
+            if (jornada.length > max)
+              max = jornada.length;
+
+            tbody.appendChild(this.rowGenerator(i, jornada));
+          }
         }
         (<HTMLInputElement>document.getElementById(`${max}j`)).checked = true;
 
@@ -222,7 +234,7 @@ export class JornadaComponent implements AfterViewInit {
     let days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
     let tr = document.createElement('tr');
     tr.setAttribute("text-align", "left");
-   
+
     let td = document.createElement('td');
     td.innerHTML = days[index];
     tr.appendChild(td);
@@ -237,15 +249,48 @@ export class JornadaComponent implements AfterViewInit {
         jornada.push("00:00-00:00");
 
       let turno = jornada[j].split("-");
-      if(turno[0].length < 5)
-        turno[0] = "0" + turno[0];
-      if(turno[1].length < 5)
-        turno[1] = "0" + turno[1];
 
+      if (turno.length == 2) {
+        if (turno[0].length < 5)
+          turno[0] = "0" + turno[0];
+        if (turno[1].length < 5)
+          turno[1] = "0" + turno[1];
+      } else {
+        turno = new Array();
+        turno.push("00:00");
+        turno.push("00:00");
+      }
       td.innerHTML = `<td><input class="t${j + 1}" value="${turno[0]}" type="time"> - <input class="t${j + 1}" value="${turno[1]}" type="time">`;
       tr.appendChild(td);
     }
     return tr;
   }
-}
 
+  generateWorkday(turns) {
+    let days = Array();
+
+    let i = 0, index = turns.length / this.option;
+    for (i; i < turns.length / this.option; i += 2) {
+      days.push(turns[i]);
+      days.push(turns[i + 1]);
+      if (this.option > 1) {
+        days.push(turns[index + i]);
+        days.push(turns[index + i + 1]);
+      }
+      if (this.option > 2) {
+        days.push(turns[index * 2 + i]);
+        days.push(turns[index * 2 + i + 1]);
+      }
+    }
+    console.log(turns);
+    console.log(days);
+  }
+  saveDay(day) {
+    fetch(this.url, {
+      method: 'POST',
+      headers: {
+      },
+      body: JSON.stringify(day)
+    });
+  }
+}
